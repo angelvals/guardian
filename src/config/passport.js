393c7d45f8bfdd5e54d9a0fe515f330afd720113
Martinez Pena <MF01B@nscorp.com>
2019-06-27 10:25:57 -0400

    Fix bug

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const passportJWT = require('passport-jwt')
+const moment = require('moment')
 
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
